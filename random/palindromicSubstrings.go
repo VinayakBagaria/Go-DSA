7dd9fe3dda8ae4695d04da71a71f943ff070ea32
VinayakBagaria <workvinayak95@gmail.com>
2023-10-27 11:28:20 +0530

    647. Palindromic Substrings

---
+++ random/palindromicSubstrings.go
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/palindromic-substrings/description/
+package random
+
+import "fmt"
+
+func countSubstrings(s string) int {
+	n := len(s)
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+
+	count := 0
+
+	for gap := 0; gap < n; gap++ {
+		for i, j := 0, gap; j < n; i, j = i+1, j+1 {
+			isEqual := s[i] == s[j]
+			if gap == 0 {
+				dp[i][j] = true
+			} else if gap == 1 {
+				dp[i][j] = isEqual
+			} else {
+				dp[i][j] = isEqual && dp[i+1][j-1]
+			}
+
+			if dp[i][j] {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func DoPalindromicSubstrings() {
+	fmt.Println(countSubstrings("abc"))
+	fmt.Println(countSubstrings("aaa"))
+}
