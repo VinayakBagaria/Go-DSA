9c82e8e72a45a8c4c19514abe347536e30a97906
VinayakBagaria <workvinayak95@gmail.com>
2023-11-02 10:35:26 +0530

    2265. Count Nodes Equal to Average of Subtree

---
+++ random/countNodesEqualToAverageOfSubtree.go
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/description/
+package random
+
+import "fmt"
+
+func averageOfSubtree(root *TreeNode) int {
+	count := 0
+
+	var dfs func(*TreeNode) (int, int)
+	dfs = func(node *TreeNode) (int, int) {
+		if node == nil {
+			return 0, 0
+		}
+
+		sum := node.Val
+		lSum, lCount := dfs(node.Left)
+		rSum, rCount := dfs(node.Right)
+
+		totalSum := sum + lSum + rSum
+		totalCount := 1 + lCount + rCount
+
+		if totalSum/totalCount == node.Val {
+			count++
+		}
+
+		return totalSum, totalCount
+	}
+
+	dfs(root)
+	return count
+}
+
+func DoCountNodesEqualToAverageOfSubtree() {
+	fmt.Println(averageOfSubtree(&TreeNode{1, nil, nil}))
+}
